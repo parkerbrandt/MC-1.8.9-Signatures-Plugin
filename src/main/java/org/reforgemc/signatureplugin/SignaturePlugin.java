350cb5a0a4659c3054cb37b0f52ea9e6db870ef0
Parker Brandt <parker.brandt101@gmail.com>
2024-10-08 14:10:18 -0500

    Initial commit

---
+++ src/main/java/org/reforgemc/signatureplugin/SignaturePlugin.java
@@ -0,0 +1,17 @@
+package org.reforgemc.signatureplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SignaturePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Hello World");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
