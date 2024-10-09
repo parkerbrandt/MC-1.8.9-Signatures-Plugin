1a474f6951cc7b5399b1506a3914b5ef4c12d7fd
Parker Brandt <parker.brandt101@gmail.com>
2024-10-08 22:38:35 -0500

    Wrote initial version of plugin

--- src/main/java/org/reforgemc/signatureplugin/SignaturePlugin.java
+++ src/main/java/org/reforgemc/signatureplugin/SignaturePlugin.java
@@ -2,12 +2,26 @@ package org.reforgemc.signatureplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main Class for the 1.8.9 Signatures Plugin
+ *
+ * @author Lucentus
+ */
 public final class SignaturePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("Hello World");
+        getLogger().info("Starting Item Signatures Plugin...");
+
+        // TODO: Need to check if the items file has already been modified
+        getLogger().info("Checking items file for signature compatibility");
+
+        try {
+
+        } catch (Exception ex) {
+
+        }
     }
 
     @Override
